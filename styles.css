c84b6c4e30ebed3e5013dc0f8131c08ecd12689f
Robert Spennato <spennato.robert@gmail.com>
2023-01-25 12:14:44 -0500

    Basic styling for etch a sketch board

---
+++ styles.css
@@ -0,0 +1,45 @@
+body {
+    width: 100%;
+    height: 100%;
+    background: rgb(180, 180, 180);
+    padding-top: 100px;
+}
+
+.game-container {
+    display: flex;
+    flex-direction: column;
+    width: 45vw;
+    height: 60vh;
+    background-color: red;
+    border-radius: 20px;
+    margin: auto;
+    padding-top: 20px;
+    text-align: center;
+    color: rgb(180, 180, 180);
+    font-size: 42px;
+    letter-spacing: 4px;
+    border: 5px solid black;
+}
+
+.drawing-board {
+    width: 75%;
+    height: 70%;
+    background-color: white;
+    margin: 20px auto;
+    border: 3px solid black;
+    border-radius: 2px;
+}
+
+.buttons-container {
+    display: flex;
+    justify-content: space-around;
+}
+
+button {
+    width: 5rem;
+    height: 5rem;
+    background-color: rgb(180, 180, 180);
+    color: black;
+    font-size: 12px;
+    margin-bottom: 10px;
+}
